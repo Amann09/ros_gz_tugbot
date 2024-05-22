b9f84b9e92126c6a6e0a64a9eb4c995e0017f5fe
Aman Ansari <100956289+Amann09@users.noreply.github.com>
2024-05-22 18:34:57 +0530

    Update tugbot_controller.py

--- ros_gz_tugbot/tugbot_controller.py
+++ ros_gz_tugbot/tugbot_controller.py
@@ -6,6 +6,14 @@ class TugbotController(Node):
     def __init__(self):
         super().__init__("tugbotController")
         self.get_logger().info("Tugbot Controller is running...")
+        self.cmd_vel_pub_ = self.create_publisher(Twist, "/model/tugbot/cmd_vel", 10)
+        self.timer_ = self.create_timer(0.5, self.send_velocity_command)
+
+    def send_velocity_command(self):
+        msg = Twist()
+        msg.linear.x = 2.0
+        msg.angular.z = 1.0
+        self.cmd_vel_pub_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
