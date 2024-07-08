844c6b037491c5abcfc069532cf8ef44a1a28f0c
Aman Ansari <100956289+Amann09@users.noreply.github.com>
2024-07-08 14:51:44 +0530

    Update tugbot_controller.py

--- ros_gz_tugbot/tugbot_controller.py
+++ ros_gz_tugbot/tugbot_controller.py
@@ -9,18 +9,6 @@ import time
 global x0 
 global y0
 
-# way = [(5, 2), (6, 4), (7, 5), (15, 10), (15, 9), (15, 8), (15, 7), (14, 7), (14, 8), (13, 8), (14, 9), (14, 10), 
-#         (14, 11), (13, 10), (12, 10), (11, 10), (11, 11), (10, 11), (10, 10), (11, 9), (10, 9), (9, 10), (9, 11), (10, 12), (9, 12), (9, 13), 
-#         (9, 14), (8, 14), (7, 14), (8, 13), (7, 13), (6, 13), (5, 13), (4, 13), (5, 12), (6, 12), (6, 11), (6, 10), (6, 9), (7, 9), (8, 9), 
-#         (7, 10), (7, 11), (7, 12), (8, 12), (8, 11), (8, 10), (9, 9), (8, 8), (9, 8), (10, 8), (9, 7), (8, 7), (7, 7), (7, 8), (6, 8), (5, 7), 
-#         (4, 6), (4, 5), (3, 5), (3, 6), (2, 7), (1, 7), (1, 6), (2, 6), (2, 5), (3, 4), (4, 4), (4, 3), (4, 2), (5, 2), (5, 3), (5, 4), (5, 5), 
-#         (5, 6), (6, 7), (7, 6), (6, 6), (6, 5), (6, 4), (6, 3), (7, 3), (7, 2), (6, 2), (5, 1), (5, 0), (6, 0), (6, 1), (7, 1), (7, 0), (8, 0), 
-#         (8, 1), (8, 2), (8, 3), (8, 4), (7, 4), (7, 5), (8, 5), (8, 6), (9, 6), (10, 7), (11, 8), (12, 9), (13, 9), (12, 8), (12, 7), (11, 7), 
-#         (10, 6), (11, 6), (12, 6), (13, 7), (13, 6), (14, 6), (15, 6), (15, 5), (15, 4), (15, 3), (15, 2), (14, 2), (14, 3), (14, 4), (14, 5), 
-#         (13, 5), (12, 5), (12, 4), (11, 4), (11, 5), (10, 5), (9, 5), (9, 4), (9, 3), (10, 4), (10, 3), (10, 2), (9, 2), (10, 1), (9, 1), (9, 0), 
-#         (10, 0), (11, 0), (11, 1), (11, 2), (11, 3), (12, 3), (12, 2), (12, 1), (12, 0), (13, 0), (14, 0), (15, 0), (15, 1), (14, 1), (13, 1), 
-#         (13, 2), (13, 3), (13, 4)]
-
 way = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 7), (1, 8), (2, 8), (3, 8), (4, 9), (4, 10), (4, 11), (4, 12), (3, 12), 
 (2, 11), (3, 11), (3, 10), (3, 9), (2, 9), (2, 10), (1, 11), (1, 10), (1, 9), (0, 8), (0, 9), (0, 10), (0, 11), (0, 12), (0, 13), (1, 13), (1, 12), 
 (2, 12), (2, 13), (3, 13), (4, 13), (5, 13), (5, 14), (4, 14), (3, 14), (2, 14), (1, 14), (0, 14), (0, 15), (1, 15), (2, 15), (3, 15), (4, 15), 
@@ -36,13 +24,18 @@ way = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 7), (1, 8), (
 (7, 3), (8, 2), (8, 3), (8, 4), (7, 5), (8, 6), (8, 5), (9, 5), (9, 6), (9, 7), (10, 7), (10, 6), (10, 5), (11, 6), (12, 6), (12, 5), (12, 4), (13, 4), (13, 3),
 (12, 3), (13, 2), (12, 2), (11, 3), (10, 3), (11, 2), (10, 2), (9, 2), (9, 3), (9, 4), (10, 4), (11, 4), (11, 5)]
 
+
+rover_way = [x for x in way if way.index(x)%2 == 0]
+
+waypoint_reached = []
+
 class PIDController(Node):
     def __init__(self):
         super().__init__('pid_controller')
         x0 = 0 # 3.90, 3.82
         y0 = 0 # 2.00, 2.09
 
-        self.waypoints = [ (x - x0, y - y0) for x, y in way ]
+        self.waypoints = [ (x - x0, y - y0) for x, y in rover_way]
         self.current_waypoint_index = 0
         self.goal_x, self.goal_y = self.waypoints[self.current_waypoint_index]
 
@@ -55,7 +48,7 @@ class PIDController(Node):
         self.ki_linear = 0.0
         self.kd_linear = 0.1
 
-        self.kp_angular = 0.5 # earlier 4.0
+        self.kp_angular = 0.50 # earlier 4.0, 0.50
         self.ki_angular = 0.0
         self.kd_angular = 0.1
 
@@ -66,8 +59,8 @@ class PIDController(Node):
         self.integral_angular = 0.0
 
     def odom_callback(self, msg: Odometry):
-        current_x = msg.pose.pose.position.x
-        current_y = msg.pose.pose.position.y
+        current_x = msg.pose.pose.position.x            # round(msg.pose.pose.position.x, 2)
+        current_y = msg.pose.pose.position.y            # round(msg.pose.pose.position.y, 2)
         current_orientation = msg.pose.pose.orientation
         # current_x = msg.pose.position.x
         # current_y = msg.pose.position.y
@@ -94,17 +87,21 @@ class PIDController(Node):
         self.prev_error_angular = error_angular
 
         # Stop if we are close to the goal
+        # if error_linear == 0.0:
         if error_linear < 0.01:
             linear_speed = 0.0
             angular_speed = 0.0
             self.get_logger().info('Waypoint reached!')
-            time.sleep(5)
+            a = msg.pose.pose.position.x
+            b = msg.pose.pose.position.y
+            waypoint_reached.append((a, b))
 
             # Move to the next waypoint
             self.current_waypoint_index += 1
             if self.current_waypoint_index < len(self.waypoints):
                 self.goal_x, self.goal_y = self.waypoints[self.current_waypoint_index]
                 self.get_logger().info(f"Moving to next waypoint: {self.goal_x}, {self.goal_y}")
+                time.sleep(5)
             else:
                 self.get_logger().info('All waypoints reached!')
                 self.publisher.publish(Twist())
@@ -138,8 +135,8 @@ class PIDController(Node):
 
     def get_yaw_from_quaternion(self, orientation):
         x, y, z, w = orientation.x, orientation.y, orientation.z, orientation.w
-        siny_cosp = 2 * (w * z + x * y)
-        cosy_cosp = 1 - 2 * (y * y + z * z)
+        siny_cosp = +2.0 * (w * z + x * y)
+        cosy_cosp = +1.0 - 2.0 * (y * y + z * z)
         return math.atan2(siny_cosp, cosy_cosp)
 
     def normalize_angle(self, angle):
